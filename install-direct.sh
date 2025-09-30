0e270f16bbf7fee936b3871835728883d8f258f9
Alex <alex@example.com>
2025-09-30 02:22:46 +0200

    Update install-direct.sh to commit 0e0daa7 with xray user fix

--- install-direct.sh
+++ install-direct.sh
@@ -21,7 +21,7 @@ mkdir -p "$TMP_DIR"
 cd "$TMP_DIR"
 
 echo "[1/4] Downloading installation files..."
-curl -sL https://github.com/cherdanalex/xray-xhttp-tls12-config/archive/1b63b25.tar.gz -o xray.tar.gz
+curl -sL https://github.com/cherdanalex/xray-xhttp-tls12-config/archive/0e0daa7.tar.gz -o xray.tar.gz
 
 echo "[2/4] Extracting files..."
 tar xzf xray.tar.gz
